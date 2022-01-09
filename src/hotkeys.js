7b8cad5d0b79b1ee727ea41a1cecd44bd6a2ba16
LeXXik <alexander.emeliyanov@gmail.com>
2022-01-09 15:29:49 +0200

    initial commit

---
+++ src/hotkeys.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        LeXXik - Hotkeys
+// @namespace   LeXXik Extensions
+// @match       https://playcanvas.com/editor/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description Additional hotkey functionality
+// ==/UserScript==
+
+/**
+ * ESC to cancel current viewport selection
+ */
+editor.once('load', function () {
+    'use strict';
+    
+    editor.call('hotkey:register', 'viewport:no-select', {
+        key: 'esc',
+        callback: function () {
+            editor.selection.clear();
+        }
+    });
+    
+});
+  
